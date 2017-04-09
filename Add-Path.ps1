d33d959c2220d222ba3e358736e85c9e010b43d4
Woongbin Kang <wbk@outlook.com>
2017-04-09 12:52:24 -0400

    Windows PATH utilities to add/remove dirs to/from PATH.
    Signed-off-by: Woongbin Kang <wbk@outlook.com>

---
+++ Add-Path.ps1
@@ -0,0 +1,47 @@
+﻿param(
+    [Parameter(mandatory=$true)]
+    [string]$BinPath,
+    [string]$Scope="User",
+    [Boolean]$AddToHead=$true
+)
+
+function Get-CanonicalPath($Path) {
+    $Path = [System.IO.Path]::Combine( ((pwd).Path), ($Path) )
+    $Path = [System.IO.Path]::GetFullPath($Path);
+    return $Path;
+}
+
+$BinPath = Get-CanonicalPath $BinPath
+
+if (-not (Test-Path $BinPath)) {
+    throw "$BinPath is not a directory"
+}
+
+[string]$UserPath = [environment]::GetEnvironmentVariable("PATH", $Scope)
+
+if (-not $UserPath) {
+    Write-Host "Setting a new PATH variable"
+    [environment]::SetEnvironmentVariable("PATH", $BinPath, $Scope)
+}
+else {
+    $CurrentPaths = [System.Collections.Generic.List[String]]$UserPath.Split(";")
+
+    foreach ($Path in $CurrentPaths) {
+        if ((Get-CanonicalPath($Path)) -eq $BinPath) {
+            Write-Host "$BinPath already in PATH. Not doing anything"
+            return
+        }
+    }
+
+    if ($AddToHead) {
+        $CurrentPaths.Insert(0, $BinPath)
+    }
+    else {
+        $CurrentPaths.add($BinPath)
+    }
+    $NewPath = [String]::Join(";", $CurrentPaths)
+
+    [environment]::SetEnvironmentVariable("PATH", $NewPath, $Scope)
+    Write-Host "New Path is $NewPath"
+}
+
---
+++ Remove-Path.ps1
@@ -0,0 +1,35 @@
+﻿param(
+    [Parameter(mandatory=$true)]
+    [string]$BinPath,
+    [string]$Scope="User"
+)
+
+function Get-CanonicalPath($Path) {
+    $Path = [System.IO.Path]::Combine( ((pwd).Path), ($Path) )
+    $Path = [System.IO.Path]::GetFullPath($Path);
+    return $Path;
+}
+
+
+[string]$UserPath = [environment]::GetEnvironmentVariable("PATH", $Scope)
+
+if (-not $UserPath) {
+    Write-Host "No PATH variable; Doing nothing."
+    return
+}
+else {
+    $CurrentPaths = [System.Collections.Generic.List[String]]$UserPath.Split(";")
+    $NewPaths = [System.Collections.Generic.List[String]]@()
+    foreach ($Path in $CurrentPaths) {
+        if ((Get-CanonicalPath($Path)) -ne (Get-CanonicalPath($BinPath))) {
+            $NewPaths.Add($Path)
+        }
+    }
+
+    
+    $NewPath = [String]::Join(";", $NewPaths)
+
+    [environment]::SetEnvironmentVariable("PATH", $NewPath, $Scope)
+    Write-Host "New Path is $NewPath"
+}
+
